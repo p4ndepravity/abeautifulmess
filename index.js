6a93ce5061731cc58edd7aae60fa44597157e245
Trent <trent.bullard@gmail.com>
2018-03-30 09:38:49 -0500

    implement passport session, install cookie session, implement cookie sessions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -7,6 +9,9 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
