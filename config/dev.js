107b9770d6d302c852a419591f0b07cb1fde6888
Trent <trent.bullard@gmail.com>
2018-03-30 09:49:26 -0500

    complete auth flow

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     "1089134761000-av4v8huvbujo8i7mr3knumik0gujpk6f.apps.googleusercontent.com",
   googleClientSecret: "PBAbUEbbovi_nh_-w8-mNUDG",
   mongoURI:
-    "mongodb://web_admin:tr3ntw3badmin@ds229609.mlab.com:29609/abeautifulmess101"
+    "mongodb://web_admin:tr3ntw3badmin@ds229609.mlab.com:29609/abeautifulmess101",
+  cookieKey: "that aint falco"
 };
