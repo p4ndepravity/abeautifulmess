bc91ffb9d25e71551a8220a799036e4a6f93eff1
Trent <trent.bullard@gmail.com>
2018-03-30 12:25:54 -0500

    scaffold header and landing, working site

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,10 +5,13 @@ import * as actions from "../actions";
 
 import Header from "./Header";
 import Landing from "./Landing";
-import Dashboard from "./Dashboard";
-import Blog from "./blog/Blog";
-import Post from "./blog/Post";
-import PostNew from "./blog/PostNew";
+// import Dashboard from "./Dashboard";
+// import Blog from "./blog/Blog";
+// import Post from "./blog/Post";
+// import PostNew from "./blog/PostNew";
+const Dashboard = () => <h2>Dashboard</h2>;
+const Blog = () => <h2>Blog</h2>;
+const PostNew = () => <h2>PostNew</h2>;
 
 class App extends Component {
   componentDidMount() {
@@ -23,7 +26,7 @@ class App extends Component {
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/posts" component={Blog} />
+            <Route exact path="/blog" component={Blog} />
             <Route path="/posts/new" component={PostNew} />
           </div>
         </BrowserRouter>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>A Beautiful Mess 101</h1>
+      Find your Ahh moment
+    </div>
+  );
+};
+
+export default Landing;
